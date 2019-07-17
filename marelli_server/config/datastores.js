6e2ba40f188deefabf0cf7470503fff7ab13f8b1
liuchengfei1993 <869275085@qq.com>
2019-07-17 17:37:51 +0800

    marelli_server dockerfile

--- marelli_server/config/datastores.js
+++ marelli_server/config/datastores.js
@@ -16,42 +16,42 @@
 module.exports.datastores = {
 
 
+  /***************************************************************************
+   *                                                                          *
+   * Your app's default datastore.                                            *
+   *                                                                          *
+   * Sails apps read and write to local disk by default, using a built-in     *
+   * database adapter called `sails-disk`.  This feature is purely for        *
+   * convenience during development; since `sails-disk` is not designed for   *
+   * use in a production environment.                                         *
+   *                                                                          *
+   * To use a different db _in development_, follow the directions below.     *
+   * Otherwise, just leave the default datastore as-is, with no `adapter`.    *
+   *                                                                          *
+   * (For production configuration, see `config/env/production.js`.)          *
+   *                                                                          *
+   ***************************************************************************/
+
+  default: {
+
     /***************************************************************************
      *                                                                          *
-     * Your app's default datastore.                                            *
+     * Want to use a different database during development?                     *
      *                                                                          *
-     * Sails apps read and write to local disk by default, using a built-in     *
-     * database adapter called `sails-disk`.  This feature is purely for        *
-     * convenience during development; since `sails-disk` is not designed for   *
-     * use in a production environment.                                         *
+     * 1. Choose an adapter:                                                    *
+     *    https://sailsjs.com/plugins/databases                                 *
      *                                                                          *
-     * To use a different db _in development_, follow the directions below.     *
-     * Otherwise, just leave the default datastore as-is, with no `adapter`.    *
+     * 2. Install it as a dependency of your Sails app.                         *
+     *    (For example:  npm install sails-mysql --save)                        *
      *                                                                          *
-     * (For production configuration, see `config/env/production.js`.)          *
+     * 3. Then pass it in, along with a connection URL.                         *
+     *    (See https://sailsjs.com/config/datastores for help.)                 *
      *                                                                          *
      ***************************************************************************/
+    adapter: 'sails-mysql',
+    url: 'mysql://root:whafd1212@sh-cdb-ffe2zdqk.sql.tencentcdb.com:61909/marelli_server',
 
-    default: {
-
-        /***************************************************************************
-         *                                                                          *
-         * Want to use a different database during development?                     *
-         *                                                                          *
-         * 1. Choose an adapter:                                                    *
-         *    https://sailsjs.com/plugins/databases                                 *
-         *                                                                          *
-         * 2. Install it as a dependency of your Sails app.                         *
-         *    (For example:  npm install sails-mysql --save)                        *
-         *                                                                          *
-         * 3. Then pass it in, along with a connection URL.                         *
-         *    (See https://sailsjs.com/config/datastores for help.)                 *
-         *                                                                          *
-         ***************************************************************************/
-        adapter: 'sails-mysql',
-        url: 'mysql://root:root@localhost:3306/marelli_server',
-
-    },
+  },
 
 
 };
